3e3e0aabf63e86eb15495714298fe037264bafac
Валерий Чирков <zx84058405@live.com>
2023-08-12 23:57:20 +0300

    all config in java.Class ? Co-authored-by: Chirkov; created SecondController.java and new views

--- src/main/java/org/chirkov/firstSpringMvcProject/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/org/chirkov/firstSpringMvcProject/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,21 +1,21 @@
 package org.chirkov.firstSpringMvcProject.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+//import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+    protected Class<?>[] getRootConfigClasses() { //не используем пока
+        return null;
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[] {SpringConfig.class}; //даём понять где брать конфинурацию вместо applicationContextMVC.xml
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[] {"/"}; //перенаправляем все запросы на данный "диспетчер сервлет"
     }
 }
