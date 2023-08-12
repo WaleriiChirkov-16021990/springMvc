008bbea1dea542f74225545553616db0e4413fb8
Валерий Чирков <zx84058405@live.com>
2023-08-12 22:33:29 +0300

    add new config (web.xml in Java.class)

---
+++ src/main/java/org/chirkov/firstSpringMvcProject/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package org.chirkov.firstSpringMvcProject.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
