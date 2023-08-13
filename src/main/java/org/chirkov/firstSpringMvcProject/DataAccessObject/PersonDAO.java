35b09b67f7b6da6947e20db8f3346981511045e7
Валерий Чирков <zx84058405@live.com>
2023-08-13 21:19:08 +0300

    realise first CRUD application. This is realised read statement;

---
+++ src/main/java/org/chirkov/firstSpringMvcProject/DataAccessObject/PersonDAO.java
@@ -0,0 +1,40 @@
+package org.chirkov.firstSpringMvcProject.DataAccessObject;
+
+import org.chirkov.firstSpringMvcProject.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class PersonDAO {
+    private static int COUNT_PEOPLE;
+    private final List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++COUNT_PEOPLE,"Tom","Johns",20, "god@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"Clark","Kent",29, "god1@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"Lao","Czi",28, "god2@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"Hero","Js",27, "god3@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"Java","Utils",26, "god4@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"Development","Kit",25, "god5@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"Java","Runtime",24, "god6@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"Google","Dec",30, "god7@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"Linux","Kali",32, "god8@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"Tod","Shark",35, "god9@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"Jerry","Mouse",40, "god10@mail.ru"));
+        people.add(new Person(++COUNT_PEOPLE,"water","Drop",50, "god11@mail.ru"));
+    }
+
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+
+
+}
---
+++ src/main/java/org/chirkov/firstSpringMvcProject/controllers/PeopleController.java
@@ -0,0 +1,36 @@
+package org.chirkov.firstSpringMvcProject.controllers;
+
+import org.chirkov.firstSpringMvcProject.DataAccessObject.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+    private final PersonDAO personDAO;
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping
+    public String index(Model model) { //получим всех людей из DAO
+        model.addAttribute("people", getPersonDAO().index());
+        return "people/index";
+    }
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) { //получаем id из GET запроса и
+        // передаем его в сигнатуру метода
+        //в теле метода получим 1 человека по его ИД из ДАО и дадим его в представление
+        model.addAttribute("person", getPersonDAO().show(id));
+        return "people/show";
+    }
+
+    public PersonDAO getPersonDAO() {
+        return personDAO;
+    }
+}
