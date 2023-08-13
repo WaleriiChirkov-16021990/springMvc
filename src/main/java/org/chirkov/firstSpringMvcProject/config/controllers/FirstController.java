2b393c5cb9f8adf4320dbde53372ec8e9a23abde
Валерий Чирков <zx84058405@live.com>
2023-08-13 16:38:23 +0300

    забираем параметры и передаем в представление с помощью шаблонизатора Thymeleaf

--- src/main/java/org/chirkov/firstSpringMvcProject/config/controllers/FirstController.java
+++ src/main/java/org/chirkov/firstSpringMvcProject/config/controllers/FirstController.java
@@ -2,6 +2,7 @@ package org.chirkov.firstSpringMvcProject.config.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,8 +20,11 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name",required = false)  String name,
-                            @RequestParam(value = "surname",required = false)  String surname) {
-        System.out.printf("Привет, " + name + " " + surname + "\n");
+                            @RequestParam(value = "surname",required = false)  String surname,
+                            Model model) {
+//        System.out.printf("Привет, " + name + " " + surname + "\n");
+
+        model.addAttribute("message","Hello, " + name + " " + surname + "!");
         return "first/hello";
     }
     @GetMapping("/bye")
