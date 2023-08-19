e81a030b32cd548f81705fdb64c372565498b0e0
Валерий Чирков <zx84058405@live.com>
2023-08-19 13:04:08 +0300

    перестала работать БД, теряется подключение после добавления пакетного обновления по 1000 человек

--- src/main/java/org/chirkov/firstSpringMvcProject/DataAccessObject/PersonDAO.java
+++ src/main/java/org/chirkov/firstSpringMvcProject/DataAccessObject/PersonDAO.java
@@ -2,11 +2,16 @@ package org.chirkov.firstSpringMvcProject.DataAccessObject;
 
 import org.chirkov.firstSpringMvcProject.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 ;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Component
 public class PersonDAO {
@@ -40,4 +45,50 @@ public class PersonDAO {
         jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
     }
 
+
+    ////////////////////////////////////////////////////////////////
+    ////////// Тестируем производительность пакетной вставки
+    ////////////////////////////////........../////////////////
+
+    public void batchUpdate() {
+        List<Person> people = createCountPeople(1000);
+        long start = System.currentTimeMillis();
+        jdbcTemplate.batchUpdate("INSERT INTO person (name, surname, age, email) VALUES (?,?,?,?)", new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setString(1, people.get(i).getName());
+                        ps.setString(2, people.get(i).getSurname());
+                        ps.setInt(3, people.get(i).getAge());
+                        ps.setString(4, people.get(i).getEmail());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return people.size();
+                    }
+                });
+        long end = System.currentTimeMillis();
+        System.out.printf("Time batchUpdate: d" ,(end -start) /1000);
+    }
+
+    public void testMultipleUpdate() {
+        List<Person> people = createCountPeople(1000);
+        long start_before = System.currentTimeMillis();
+        for (Person person : people
+             ) {
+            jdbcTemplate.update("INSERT INTO person (name, surname, age, email) VALUES (?,?,?,?)", person.getName(), person.getSurname(), person.getAge(), person.getEmail() );
+        }
+        long end_after = System.currentTimeMillis();
+        System.out.printf("Время обнослвения поочереди = %d", (end_after-start_before)/1000);
+    }
+
+    private List<Person> createCountPeople(int count) {
+        List<Person> people = new ArrayList<Person>();
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            people.add(new Person(i,"John" + String.valueOf(i), "Doe" + String.valueOf(i),random.nextInt(4,120),"john.doe@gmail.com"));
+        }
+        System.out.printf(String.valueOf(people.size()));
+        return people;
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,5 +3,6 @@
 <h2>Hello World!</h2><br>
 <a href="/people/new">new</a><br>
 <a href="/people">people</a><br>
+<a href="/test-batch-update">test-batch-update</a><br>
 </body>
 </html>
